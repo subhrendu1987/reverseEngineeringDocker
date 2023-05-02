fce2958816e73ffa05997c65b7903a516b1b5db5
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-05-02 12:55:55 +0530

    modified:   DockerFileCreator.py 	modified:   README.md 	renamed:    runopts -> dockerRunOpts

--- DockerFileCreator.py
+++ DockerFileCreator.py
@@ -2,7 +2,19 @@ import os, sys, argparse, random, re, subprocess, docker, tarfile
 #############################################################################
 client = docker.from_env()
 #############################################################################
-def copy_to(src, dst,untar=True):
+def untar(filename,isdir=True):
+    file = tarfile.open(filename+".tar")
+    if(isdir):
+        file.extractall(filename)
+    else:
+        fname=os.path.basename(filename)
+        pathname=os.path.dirname(filename)
+        file.extract(fname,path=pathname)
+    file.close()
+    if(not isdir):
+        os.remove(filename+".tar")
+#############################################################################
+def copy_to(src, dst):
     to_container=True
     if(":" in dst):
         name, dst = dst.split(':')
@@ -25,9 +37,6 @@ def copy_to(src, dst,untar=True):
         for chunk in bits:
             f.write(chunk)
         f.close()
-    if(untar):
-        pass
-        os.system("tar -x %s"%(filename+".tar"))
     return(filename)
 #############################################################################
 def parse_args():
@@ -35,8 +44,8 @@ def parse_args():
     
     parser.add_argument('--image', '-i',
                         action="store",
-                        required=True,
-                        help="Image name with tag",
+                        #required=True, # comment if debugging with ipython
+                        help="Image name with tag. e.g myimage:latest",
                         default="")
     parser.add_argument('--outputpath', '-o',
                         action="store",
@@ -59,14 +68,15 @@ args=parse_args()
 ''' STUB
 args.image='openvswitch/ovs:2.12.0_debian_4.15.0-66-generic'
 args.runcmd="-itd --net=host"
-args.internalcmd="ovsdb-server"
+args.execute="ovsdb-server"
 '''
-with open("runopts") as myfile:
+with open("dockerRunOpts") as myfile:
     args.runcmd=myfile.readlines()[0]
 print(args)
 #############################################################################
 IMAGEID=exec_cmd("sudo docker images %s --format=\"{{.ID}}\"" % args.image)
-DOCKER_CMD="sudo docker run %s --name=tempContainer %s %s" %(args.runcmd,IMAGEID,args.execute)
+DOCKER_RUN_CMD="sudo docker run %s --name=tempContainer %s %s" %(args.runcmd,IMAGEID,args.execute)
+os.system(DOCKER_RUN_CMD)
 DOCKER_FILE=args.outputpath+"/Dockerfile"
 if(len(IMAGEID.split())>1):
     sys.exit('(%d) Images found. Please provide a valid docker image name'%len(IMAGEID.split()))
@@ -85,26 +95,35 @@ for i,line in enumerate(lines):
 keys=list(hashed_key.keys())
 keys.reverse()
 for k in keys:
-    print(k)
     curr_dir=os.getcwd()
     stmt=hashed_key[k]
-    if(stmt[0]=="COPY"):
-        target=stmt[3]
-        dest=os.path.basename(target)
-        try:
-            filename=copy_to("tempContainer:"+target,curr_dir+"/"+args.outputpath+"/files/"+dest)
-            if(filename):
-                stmt[2]=args.outputpath+"/files/"+filename
-                del stmt[1]
-                lines[k]= " ".join(stmt)+"\n"
-        except:
-            pass
-    elif(stmt[0]=="ADD"):
-        pass
-    else:
+    print(stmt[0],stmt[3])
+    target=stmt[3]
+    dest=os.path.basename(target)
+    dest = "root" if(dest=="") else dest
+    try:
+        filename=copy_to("tempContainer:"+target,curr_dir+"/"+args.outputpath+"/files/"+dest)
+        if(filename):
+            if(stmt[0]=="ADD"):
+                stmt[2]="./files/"+os.path.basename(filename)+".tar"
+            elif(stmt[0]=="COPY"):
+                stmt[2]="./files/"+os.path.basename(filename)
+            else:
+                pass
+            del stmt[1]
+            lines[k]= " ".join(stmt)+"\n"
+            if(stmt[0]=="COPY"):
+                untar(filename,isdir=os.path.isdir(target))
+            else:
+                pass
+    except:
         pass
     os.chdir(curr_dir)
 ##########################################
-with open(DOCKER_FILE+".tmp", 'w') as f:
+with open(DOCKER_FILE, 'w') as f:
     for line in lines:
         f.write(line)
+##########################################
+print("## Clean up the system")
+os.system("sudo docker kill tempContainer")
+os.system("sudo docker rm tempContainer")
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# reverseEngineeringDocker
+# Create Dockerfile from images
+## Help options
+`python3 DockerFileCreator.py --help`
+## Example scenario
+Say a docker image name `openvswitch/ovs` with tag `2.12.0_debian_4.15.0-66-generic` requires the following command to execute
+```
+docker run -itd --net=host --name=ovsdb-server openvswitch/ovs: 2.11.2_debian ovsdb-server
+```
+First add `-itd --net=host` in `dockerRunOpts` and then use the following command to generate the Dockerfile and relevant files inside a folder `ovs`
+
+```
+python3 DockerFileCreator.py -i openvswitch/ovs:2.12.0_debian_4.15.0-66-generic -e ovsdb-server -o ovs
+```
